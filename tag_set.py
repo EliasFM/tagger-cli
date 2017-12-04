2ccb1458b693fb258ff79e7fabf48ae90ec9a58f
Alex Tan <alextan@uw.edu>
2017-12-04 14:40:33 -0800

    Updated to tag set

--- tag_set.py
+++ tag_set.py
@@ -1,8 +1,8 @@
 class tags:
-    list = ["Anti-Subject", "Pro-Subject",
-            "Anti-Liberal", "Pro-Liberal",
-            "Anti-Conservative", "Pro-Conservative",
-            "Attacks News Source", "Praises News Source",
-            "Attacks Participant", "Praises Participant",
-            "Politically Neutral", "Politically Ambiguous",
-            "Irrelevant", "Relevant"]
+    list = ["Relevant", "Irrelevant",
+            "Politically Ambiguous", "Politically Neutral",
+            "Pro-Liberal", "Pro-Conservative",
+            "Anti-Liberal", "Anti-Conservative",
+            "Pro-Subject", "Anti-Subject",
+            "Praises News Source", "Attacks News Source",
+            "Praises Participant", "Attacks Participant"]
